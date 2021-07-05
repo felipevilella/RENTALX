81595f07609ecfb80eb0380adbbba12197675a20
Felipe <felipevilellaalves@hotmail.com>
2021-07-05 19:00:58 -0300

    Create and model, Repository and implementation DTO

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,17 +1,28 @@
 import {request, response, Router} from 'express';
+import {CategoriesRepository } from '../repositories/categoriesRepository';
 
 const categoriesRoutes = Router();
-const categories = [];
 
-categoriesRoutes.post("/categories", (request, response) => {
+categoriesRoutes.post("/", (request, response) => {
   let {name, description} = request.body;
+  const categoriesRepository = new CategoriesRepository();
+  let categoryAlreadyExists = categoriesRepository.findByName(name);
 
-  categories.push({
-    name,
-    description
-  })
+  if(categoryAlreadyExists) {
+    return response.status(400).send("Category already exist");
+  }
 
+  categoriesRepository.create({name , description});
+  
   return response.status(201).send();
+});
+
+categoriesRoutes.get('/', (request, response) => {
+  const categoriesRepository = new CategoriesRepository();
+
+  let categories = categoriesRepository.list();
+
+  return response.json(categories);
 });
 
 export {categoriesRoutes};
